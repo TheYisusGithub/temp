8aab7edfed8e45cccb54035218c7dc209f7fa020
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-09-08 05:54:19 -0400

    Add comment

--- src/main/java/kanbanflow/ui/pages/AddTask.java
+++ src/main/java/kanbanflow/ui/pages/AddTask.java
@@ -3,6 +3,9 @@ package kanbanflow.ui.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+/**
+ *Class let add new task in the board.
+ */
 public class AddTask {
     @FindBy(className = "columnHeader-addTask")
     private WebElement addTask;
@@ -11,6 +14,11 @@ public class AddTask {
     @FindBy(className = "addTaskDialog-buttonWrapper")
     private WebElement sendSave;
 
+    /**
+     *Method to let a send a description to the task.
+     *
+     * @param description this is an argument.
+     */
     public void setAddNewToDo(String description) {
         addTask.click();
         addTaskName.sendKeys(description);
