e697261b8619e2f6d0ecae18afe94b7b3be3942d
TheYisusGithub <Jesus.Menacho@fundacion-jala.org>
2019-09-08 19:26:34 -0400

    Update class

--- src/main/java/kanbanflow/ui/pages/Board.java
+++ src/main/java/kanbanflow/ui/pages/Board.java
@@ -1,18 +1,22 @@
 package kanbanflow.ui.pages;
 
+import kanbanflow.ui.BasePage;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 /**
  *Class let add new task in the board.
  */
-public class AddTask {
-    @FindBy(className = "columnHeader-addTask")
-    private WebElement addTask;
-    @FindBy(className = "addTaskDialog-formRow addTaskDialog-nameWrapper")
+public class Board extends BasePage {
+    @FindBy(xpath = "//h2[contains(@title, 'To-do')]/parent::div/following-sibling::button")
+    private WebElement addTaskButton;
+    @FindBy(className = "addTaskDialog-name")
     private WebElement addTaskName;
     @FindBy(className = "addTaskDialog-buttonWrapper")
     private WebElement sendSave;
+    @FindBy(className = "addTaskDialog-wrapper")
+    private WebElement modal;
+
 
     /**
      *Method to let a send a description to the task.
@@ -20,8 +24,11 @@ public class AddTask {
      * @param description this is an argument.
      */
     public void setAddNewToDo(String description) {
-        addTask.click();
+        addTaskButton.click();
         addTaskName.sendKeys(description);
         sendSave.click();
+    }
+    public boolean isBoardPage(){
+        return driver.getCurrentUrl().contains("board");
     }
 }
